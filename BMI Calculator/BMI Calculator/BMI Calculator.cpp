83af7ef2999ff0e4e18c650ec4c6c6049416da26
Crespo <60497466+CREZSPO@users.noreply.github.com>
2023-05-01 21:27:26 -0600

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/BMI Calculator.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+void main(){
+
+	// BMI Calculator  
+	// weight (kg) < height (m)
+
+	// Underweight < 18.5
+	// Normal weight 18.5 - 24.9
+	// Overweight > 25
+
+	float weight, height, bmi;
+	cout << "Enter your Weight (kg) and Height (m)" << endl;
+	cin >> weight >> height;
+	bmi = weight / (height * height);
+
+	if(bmi < 18.5){
+		cout << "Underweight" << endl;
+	}
+	else if(bmi > 25) {
+		cout << "Overweight" << endl;
+	}
+	else {
+		cout << "Normal weight" << endl;
+	}
+
+	cout << "Your bmi is " << bmi; 
+
+	system("pause>0");
+}
