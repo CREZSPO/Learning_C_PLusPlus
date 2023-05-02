b31b85eb472a073c5aa9da6d764bb3077f2e7a94
Crespo <60497466+CREZSPO@users.noreply.github.com>
2023-05-01 21:26:47 -0600

    Add files via upload

---
+++ ChipeoLetras/ChipeoLetras/ChipeoLetras.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Hello World!\n";
+    cout << "My name is CREZSPO\n";
+    cout << "Today is Wednesday\n";
+
+    system("pause>0");
+}
